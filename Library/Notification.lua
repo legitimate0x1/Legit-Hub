26e82f9ca6a966629bc47148a2b3816116eb9a1d
legitimate0x1 <120129171+legitimate0x1@users.noreply.github.com>
2023-04-15 12:08:23 +0300

    Create Notification.lua

---
+++ Library/Notification.lua
@@ -0,0 +1,53 @@
+-- Made by ArasOfficial#8439
+
+if not rconsolename then
+    return warn("[Notification-Library]: Your exploit is not supported! Missing Function: rconsolename()")
+end
+
+if not rconsoleprint then
+    return warn("[Notification-Library]: Your exploit is not supported! Missing Function: rconsoleprint()")
+end
+
+if not getgenv then
+    return warn("[Notification-Library]: Your exploit is not supported! Missing Function: getgenv()")
+end
+
+getgenv().Notify = function(ConsoleName, Type, Message)
+    rconsolename(ConsoleName)
+  
+    if Type == "Info" then
+        rconsoleprint("[")
+        rconsoleprint("@@BLUE@@")
+        rconsoleprint("INFO")
+        rconsoleprint("@@LIGHT_GRAY@@")
+        rconsoleprint("]: ")
+        rconsoleprint(Message .. " \n")
+    end
+
+    if Type == "Warning" then
+        rconsoleprint("[")
+        rconsoleprint("@@YELLOW@@")
+        rconsoleprint("WARNING")
+        rconsoleprint("@@LIGHT_GRAY@@")
+        rconsoleprint("]: ")
+        rconsoleprint(Message .. " \n")
+    end
+
+    if Type == "Error" then
+        rconsoleprint("[")
+        rconsoleprint("@@RED@@")
+        rconsoleprint("ERROR")
+        rconsoleprint("@@LIGHT_GRAY@@")
+        rconsoleprint("]: ")
+        rconsoleprint(Message .. " \n")
+    end
+
+    if Type == "Success" then
+        rconsoleprint("[")
+        rconsoleprint("@@GREEN@@")
+        rconsoleprint("SUCCESS")
+        rconsoleprint("@@LIGHT_GRAY@@")
+        rconsoleprint("]: ")
+        rconsoleprint(Message .. " \n")
+    end
+end
