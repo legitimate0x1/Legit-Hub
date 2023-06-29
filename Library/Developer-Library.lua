a129cbeb0b59467344b25c3037d3ef2de5bdcd8e
legitimate0x1 <120129171+legitimate0x1@users.noreply.github.com>
2023-06-29 16:42:11 +0300

    Update Developer-Library.lua

--- Library/Developer-Library.lua
+++ Library/Developer-Library.lua
@@ -1,163 +1,161 @@
--- Made by ArasOfficial#8439
+-- Made by legitimate0x1!
 
-getgenv().Workspace = game:GetService("Workspace")
-getgenv().UserInputService  = game:GetService("UserInputService")
-getgenv().ReplicatedStorage = game:GetService("ReplicatedStorage")
-getgenv().StarterPlayer = game:GetService("StarterPlayer")
-getgenv().StarterPack = game:GetService("StarterPack")
-getgenv().StarterGui = game:GetService("StarterGui")
-getgenv().TeleportService = game:GetService("TeleportService")
-getgenv().CoreGui = game:GetService("CoreGui")
-getgenv().TweenService = game:GetService("TweenService")
-getgenv().HttpService = game:GetService("HttpService")
-getgenv().TextService = game:GetService("TextService")
-getgenv().MarketplaceService = game:GetService("MarketplaceService")
-getgenv().Chat = game:GetService("Chat")
-getgenv().Teams = game:GetService("Teams")
-getgenv().SoundService = game:GetService("SoundService")
-getgenv().Lighting = game:GetService("Lighting")
-getgenv().ScriptContext = game:GetService("ScriptContext")
-getgenv().Stats = game:GetService("Stats")
-getgenv().RunService = game:GetService("RunService")
-getgenv().Players = game:GetService("Players")
-getgenv().LocalPlayer = Players.LocalPlayer
-getgenv().Mouse = LocalPlayer:GetMouse ""
-getgenv().Camera = Workspace:FindFirstChildWhichIsA("Camera")
-
-getgenv().Character = function(Player)
-    if not Player then
-        return LocalPlayer.Character
-    end
-
-    return Player.Character
+if not getgenv then
+	return
+elseif typeof(getgenv) ~= "function" then
+	return
 end
 
-getgenv().Humanoid = function(Player)
-    if not Player then
-        return LocalPlayer.Character.Humanoid
-    end
+getgenv().LH = {}
 
-    return Player.Character.Humanoid
+LH.Workspace = game:GetService("Workspace")
+LH.UserInputService  = game:GetService("UserInputService")
+LH.ReplicatedStorage = game:GetService("ReplicatedStorage")
+LH.StarterPlayer = game:GetService("StarterPlayer")
+LH.StarterPack = game:GetService("StarterPack")
+LH.StarterGui = game:GetService("StarterGui")
+LH.TeleportService = game:GetService("TeleportService")
+LH.CoreGui = game:GetService("CoreGui")
+LH.TweenService = game:GetService("TweenService")
+LH.HttpService = game:GetService("HttpService")
+LH.TextService = game:GetService("TextService")
+LH.MarketplaceService = game:GetService("MarketplaceService")
+LH.Chat = game:GetService("Chat")
+LH.Teams = game:GetService("Teams")
+LH.SoundService = game:GetService("SoundService")
+LH.Lighting = game:GetService("Lighting")
+LH.ScriptContext = game:GetService("ScriptContext")
+LH.Stats = game:GetService("Stats")
+LH.RunService = game:GetService("RunService")
+LH.Players = game:GetService("Players")
+LH.LocalPlayer = LH.Players.LocalPlayer
+LH.Mouse = LH.LocalPlayer:GetMouse ""
+LH.Camera = LH.Workspace:FindFirstChildWhichIsA("Camera")
+
+LH.Character = function(Player)
+	if not Player then
+		return LH.LocalPlayer.Character
+	end
+
+	return Player.Character
 end
 
-getgenv().HumanoidRootPart = function(Player)
-    if not Player then
-        return LocalPlayer.Character.HumanoidRootPart
-    end
-
-    return Player.Character.HumanoidRootPart
+LH.Humanoid = function(Player)
+	return LH.Character(Player).Humanoid
 end
 
-getgenv().IsR6 = function(Player)
-    if Humanoid(Player).RigType == Enum.HumanoidRigType.R6 then
-        return true
-    end
+LH.HumanoidRootPart = function(Player)
+	return LH.Character(Player).HumanoidRootPart
 end
 
-getgenv().equiptools = function()
-    for i,v in pairs(LocalPlayer.Backpack) do
-        if v:IsA("Tool") then
-            v.Parent = Character()
-        end
-    end
+LH.IsR6 = function(Player)
+	if LH.Humanoid(Player).RigType == Enum.HumanoidRigType.R6 then
+		return true
+	end
 end
 
-getgenv().filter = function(tbl, ret) -- By Fates.
-    if (type(tbl) == 'table') then
-        local new = {}
-        for i, v in next, tbl do
-            if (ret(i, v)) then
-                new[#new + 1] = v
-            end
-        end
-        return new
-    end
+LH.equiptools = function()
+	for i,v in pairs(LH.LocalPlayer.Backpack) do
+		if v:IsA("Tool") then
+			v.Parent = Character()
+		end
+	end
 end
 
-getgenv().equiptools2 = function()
-    local Tools = filter(game.GetChildren(LocalPlayer.Backpack), function(i, Child) -- By Fates.
-         return game.IsA(Child, "Tool")
-    end)
-
-    for i, v in next, Tools do
-        v.Parent = LocalPlayer.Character
-    end
+LH.filter = function(tbl, ret) -- By Fates.
+	if (type(tbl) == 'table') then
+		local new = {}
+		for i, v in next, tbl do
+			if (ret(i, v)) then
+				new[#new + 1] = v
+			end
+		end
+		return new
+	end
 end
 
-getgenv().ReplaceCharacter = function(Character) -- By Fates.
-    local Char = LocalPlayer.Character or Character() or Character
-    local Model = Instance.new("Model")
-    LocalPlayer.Character = Model
-    LocalPlayer.Character = Char
-    Model:Destroy()
-    return Char
+LH.equiptools2 = function() -- By Fates.
+	local Tools = filter(game.GetChildren(LH.LocalPlayer.Backpack), function(i, Child)
+		return game.IsA(Child, "Tool")
+	end)
+
+	for i, v in next, Tools do
+		v.Parent = LH.Character()
+	end
 end
 
-getgenv().ReplaceHumanoid = function(Hum, R) -- By Fates.
-    local Humanoid = Hum or Humanoid()
-    local NewHumanoid = Humanoid:Clone()
-    if (R) then
-        NewHumanoid.Name = "1"
-    end
-    NewHumanoid.Parent = Humanoid.Parent -- NewHumanoid.Parent = Character()
-    NewHumanoid.Name = Humanoid.Name
-    Camera.CameraSubject = NewHumanoid
-    Humanoid:Destroy()
-    return NewHumanoid
+LH.ReplaceCharacter = function(Character) -- By Fates.
+	local Char = LH.LocalPlayer.Character or LH.Character() or Character
+	local Model = Instance.new("Model")
+	LH.LocalPlayer.Character = Model
+	LH.LocalPlayer.Character = Char
+	Model:Destroy()
+	return Char
 end
 
-getgenv().activatetools = function(Number)
-    for i,v in pairs(Character():GetChildren()) do
-        if v:IsA("Tool") then
-            for i=1, Number do
-                v:Activate()
-                wait()
-            end
-        end
-    end
+LH.ReplaceHumanoid = function(Hum, R) -- By Fates.
+	local Humanoid = Hum or LH.Humanoid()
+	local NewHumanoid = Humanoid:Clone()
+	if (R) then
+		NewHumanoid.Name = "1"
+	end
+	NewHumanoid.Parent = Humanoid.Parent -- NewHumanoid.Parent = Character()
+	NewHumanoid.Name = Humanoid.Name
+	LH.Camera.CameraSubject = NewHumanoid
+	Humanoid:Destroy()
+	return NewHumanoid
 end
 
-getgenv().GetServers = function(PlaceId)
-	return HttpService:JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data
+LH.activatetools = function(Number)
+	for i,v in pairs(LH.Character():GetChildren()) do
+		if v:IsA("Tool") then
+			for i=1, Number do
+				v:Activate()
+				wait()
+			end
+		end
+	end
 end
 
-getgenv().TeleportServer = function(PlaceId, JobId)
-	if not PlaceId and not JobId then return warn("[Error]: Teleport failed!") end
-	
+LH.GetServers = function(PlaceId)
+	return LH.HttpService:JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data
+end
+
+LH.TeleportServer = function(PlaceId, JobId)
+	if not PlaceId and not JobId then return end
+
 	if not JobId then
-		TeleportService:Teleport(PlaceId)
+		LH.TeleportService:Teleport(PlaceId)
 	else
-		TeleportService:TeleportToPlaceInstance(PlaceId, JobId, LocalPlayer)
+		LH.TeleportService:TeleportToPlaceInstance(PlaceId, JobId, LH.LocalPlayer)
 	end
 end
 
-getgenv().Rejoin = function()
-	if #Players:GetPlayers() == 1 then
-		LocalPlayer:Kick("Rejoining...")
-		TeleportServer(game.PlaceId)
+LH.Rejoin = function()
+	if #LH.Players:GetPlayers() == 1 then
+		LH.LocalPlayer:Kick("Rejoining...")
+		LH.TeleportServer(game.PlaceId)
 	else
-		TeleportServer(game.PlaceId, game.JobId)
+		LH.TeleportServer(game.PlaceId, game.JobId)
 	end
 end
 
-getgenv().Serverhop = function()
+LH.Serverhop = function()
 	local ServerList = {}
 
-	for _, v in ipairs(GetServers(game.PlaceId)) do
+	for _, v in ipairs(LH.GetServers(game.PlaceId)) do
 		if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
 			ServerList[#ServerList + 1] = v.id
 		end
 	end
 
 	if #ServerList > 0 then
-		TeleportServer(game.PlaceId, ServerList[math.random(1, #ServerList)])
-	else
-		if LH_Notify then LH_Notify({ title = "Legit Hub", message = "No servers found!", duration = 5}) else print("[Error]: No servers found!") end
+		LH.TeleportServer(game.PlaceId, ServerList[math.random(1, #ServerList)])
 	end
 end
 
-getgenv().randomString = function() -- By IY
-	local length = math.random(10,20)
+LH.randomString = function(A_1, A_2) -- By IY
+	local length = math.random(A_1, A_2)
 	local array = {}
 	for i = 1, length do
 		array[i] = string.char(math.random(32, 126))
